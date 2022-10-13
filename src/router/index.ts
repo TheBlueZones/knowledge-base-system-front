0e93fd58df6f7a9fe78821e52146b72449b34ade
yide <3014453996@qq.com>
2022-10-13 14:50:44 +0800

    修改电子书管理页面布局

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from "@/views/Home.vue";
+import AdminEbook from "@/views/admin/AdminEbook.vue";
+import About from "@/views/About.vue";
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  }
+  }, {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/admin/ebook',
+    name: 'AdminEbook',
+    component: AdminEbook
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>111111111111</h1>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "About"
+})
+</script>
+
+<style scoped>
+
+</style>
