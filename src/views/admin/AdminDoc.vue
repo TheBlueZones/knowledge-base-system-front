bbf99278c39f5a660d3f5c61ccf9e60b27e84c31
yide <3014453996@qq.com>
2022-10-19 19:03:07 +0800

    删除某个文档时，将旗下所有子文档全部删除。

--- src/views/admin/AdminDoc.vue
+++ src/views/admin/AdminDoc.vue
@@ -274,12 +274,47 @@ export default defineComponent({
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
     }
+
+    const ids: Array<string> = [];
+
+    const getDeleteIds = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("delete", node);
+          // 将目标ID放入结果集ids
+          // node.disabled = true;
+          ids.push(id);
+      /*    deleteIds.push(id);
+          deleteNames.push(node.name);*/
+
+          // 遍历所有子节点
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              getDeleteIds(children, children[j].id)
+            }
+          }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            getDeleteIds(children, id);
+          }
+        }
+      }
+    };
+
     /**
      * 删除
      * */
     const handleDelete = (id: number) => {
+      getDeleteIds(level1.value,id);
       /*这里直接拼接字符串*/
-      axios.delete("/doc/delete/" + id).then((response) => {
+      axios.delete("/doc/delete/" + ids.join(",")).then((response) => {
         const data = response.data; // data = commonResp
         if (data.success) {
           // 重新加载列表
