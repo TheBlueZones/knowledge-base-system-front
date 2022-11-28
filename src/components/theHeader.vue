d40fcc4ccbe6e22623fc77d2bec537f124a7dcc1
yide <3014453996@qq.com>
2022-11-28 16:24:54 +0800

    修复首页文档显示问题

--- src/components/theHeader.vue
+++ src/components/theHeader.vue
@@ -2,7 +2,6 @@
   <a-layout-header class="header">
     <div class="logo"/>
     <a-menu
-        v-model:selectedKeys="selectedKeys1"
         theme="dark"
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
@@ -10,32 +9,167 @@
       <a-menu-item key="/">
         <router-link to="/">首页</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/ebook">
+
+      <a-menu-item key="/admin/user" v-if="user.id">
+        <router-link to="/admin/user">用户管理</router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin/ebook" v-if="user.id">
         <router-link to="/admin/ebook">电子书管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/category">
+      <!--      :style="user.id? {} : {display:'none'}"-->
+      <a-menu-item key="/admin/category" v-if="user.id">
         <router-link to="/admin/category">分类管理</router-link>
       </a-menu-item>
       <a-menu-item key="about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
+      <a-menu-item key="greet" v-if="user.id" class="login-menu">
+        <a >
+          <span>您好哇：{{ user.name }}</span>
+        </a>
+      </a-menu-item>
+      <a-menu-item key="logout" v-if="user.id" class="login-menu">
+        <a-popconfirm
+            title="是否退出登录？"
+            ok-text="是"
+            cancel-text="否"
+            @confirm="logout()"
+        >
+          <a >
+            <span>退出登录</span>
+          </a>
+        </a-popconfirm>
+      </a-menu-item>
+
+
+      <a-menu-item key="login" v-if="!user.id" class="login-menu">
+        <a @click="showLoginModal">
+          <span>登录</span>
+        </a>
+      </a-menu-item>
+      <!--      <a class="login-menu" v-show="!user.id" @click="showLoginModal">-->
+      <!--        <span>登录</span>-->
+      <!--      </a>-->
     </a-menu>
+
+    <a-modal
+        title="登录"
+        v-model:visible="loginModalVisible"
+        :confirm-loading="loginModalLoading"
+        @ok="login"
+    >
+      <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+        <a-form-item label="登录名">
+          <a-input v-model:value="loginUser.loginName"/>
+        </a-form-item>
+        <a-form-item label="密码">
+          <a-input v-model:value="loginUser.password" type="password"/>
+        </a-form-item>
+      </a-form>
+    </a-modal>
+    \
+
   </a-layout-header>
+
 </template>
 
 <script lang="ts">
-import {ref} from "vue";
 
-export default {
-  name: "theHeader",
+import {defineComponent, ref, computed} from 'vue';
+import axios from 'axios';
+import {message} from 'ant-design-vue';
+import store from "@/store";
+
+declare let hexMd5: any;
+declare let KEY: any;
+
+export default defineComponent({
+  name: 'the-header',
   setup() {
-    return {
-      selectedKeys1: ref<string[]>(['1']),
+
+    // 登录后保存
+    const user = computed(() => store.state.user);
+    // const user = ref();
+    // user.value = {};//初始化给一个空对象，避免空指针异常
+
+    // 用来登录
+    const loginUser = ref({
+      loginName: "test",
+      password: "test"
+    });
+    const loginModalVisible = ref(false);
+    const loginModalLoading = ref(false);
+    const showLoginModal = () => {
+      loginModalVisible.value = true;
     };
-  },
-}
+
+    // 登录
+    const login = () => {
+      console.log("开始登录");
+      loginModalLoading.value = true;
+      loginUser.value.password = hexMd5(loginUser.value.password + KEY);
+      axios.post('/user/login', loginUser.value).then((response) => {
+        loginModalLoading.value = false;
+        const data = response.data;
+        if (data.success) {
+          loginModalVisible.value = false;
+          message.success("登录成功！");
+
+          store.commit("setUser", data.content);//commit触发mutations
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    // 退出登录
+    const logout = () => {
+      console.log("退出登录开始");
+      axios.get('/user/logout/'+user.value.token).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          message.success("退出登录成功！");
+          store.commit("setUser", {});
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    return {
+      loginModalVisible,
+      loginModalLoading,
+      showLoginModal,
+      loginUser,
+      login,
+      logout,
+      user
+    }
+  }
+});
 </script>
 
 <style scoped>
+.login-menu {
 
+  color: white;
+  margin-left: 100px;
+
+}
+
+/*.login-menu::before {*/
+/*  content: none;*/
+/*}*/
+/*.login-menu::after {*/
+/*  content: none;*/
+/*}*/
+.logo {
+  width: 120px;
+  height: 31px;
+  /*background: rgba(255, 255, 255, 0.2);*/
+  /*margin: 16px 28px 16px 0;*/
+  float: left;
+  color: white;
+  font-size: 18px;
+}
 </style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,4 +1,4 @@
- <template>
+<template>
   <a-layout>
     <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
       <h3 v-if="level1.length === 0">对不起，找不到相关文档！</h3>
@@ -8,22 +8,35 @@
               v-if="level1.length > 0"
               :tree-data="level1"
               @select="onSelect"
-              :field-names="{label: 'name',value: 'id', children: 'children' }"
+              :field-names="{title: 'name', key: 'id', value: 'id' }"
               :defaultExpandAll="true"
               :default-expand-all="true"
+              :defaultSelectedKeys="defaultSelectedKeys"
           >
           </a-tree>
         </a-col>
         <a-col :span="18">
           <div>
-            <h2>{{doc.name}}</h2>
+            <h2>{{ doc.name }}</h2>
             <div>
-              <span>阅读数：{{doc.viewCount}}</span>&nbsp;&nbsp;
-              <span>点赞数：{{doc.voteCount}}</span>
+              <span>阅读数：{{ doc.viewCount }}</span>&nbsp;&nbsp;
+              <span>点赞数：{{ doc.voteCount }}</span>
             </div>
-            <a-divider />
+            <a-divider/>
           </div>
-          <div :innerHTML="html"></div>
+          <div style="border: 1px solid #ccc">
+            <Editor
+                style="height: 500px; overflow-y: hidden;"
+                v-model="html"
+                :mode="mode"
+                @onCreated="handleCreated"
+            />
+          </div>
+          <div class="vote-div">
+            <a-button type="primary" shape="round" :size="'large'" @click="vote">
+              <like-two-tone/>&nbsp;点赞：{{ doc.voteCount }}
+            </a-button>
+          </div>
         </a-col>
       </a-row>
     </a-layout-content>
@@ -31,19 +44,20 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, ref} from "vue";
+import {defineComponent, onMounted, ref, shallowRef} from "vue";
 import axios from "axios";
 import {Tool} from "@/util/tool";
 import {message} from "ant-design-vue";
 import {useRoute} from "vue-router";
-
+import '@wangeditor/editor/dist/css/style.css' // 引入 css
+import { Editor } from '@wangeditor/editor-for-vue'
 export default defineComponent({
   name: 'Doc',
-  components: {},
+  components: { Editor},
   setup() {
     const route = useRoute();
     const docs = ref();
-    const html = ref();
+
     const defaultSelectedKeys = ref();
     defaultSelectedKeys.value = [];
 
@@ -53,14 +67,23 @@ export default defineComponent({
     const doc = ref();
     doc.value = {};
 
+
+    /*富文本*/
+    // 编辑器实例，必须用 shallowRef
+    const editorRef = shallowRef()
+    const html = ref()
+    const handleCreated = (editor: any) => {
+      editorRef.value = editor // 记录 editor 实例，重要！
+    }
     /**
      * 内容查询
      **/
     const handleQueryContent = (id: number) => {
-      axios.get("/doc/find-content/" + id).then((response) => {
+      axios.get("/doc/findContent/" + id).then((response) => {
         const data = response.data;
         if (data.success) {
           html.value = data.content;
+          console.log(html.value);
         } else {
           message.error(data.message);
         }
@@ -76,44 +99,128 @@ export default defineComponent({
         handleQueryContent(selectedKeys[0]);
       }
     };
-  const handleQuery = () => {
-    axios.get("/doc/all" + route.query.ebookId).then((response) => {
-      const data = response.data;
-      if (data.success) {
-        docs.value = data.content;
+    const handleQuery = () => {
+      axios.get("/doc/all/" + route.query.ebookId).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          docs.value = data.content;
 
-        level1.value = [];
-        level1.value = Tool.array2Tree(docs.value, 0);
+          level1.value = [];
+          level1.value = Tool.array2Tree(docs.value, 0);
 
-        if (Tool.isNotEmpty(level1)) {
-          defaultSelectedKeys.value = [level1.value[0].id];
-          handleQueryContent(level1.value[0].id);
-          // 初始显示文档信息
-          doc.value = level1.value[0];
+          if (Tool.isNotEmpty(level1)) {
+            defaultSelectedKeys.value = [level1.value[0].id];
+            handleQueryContent(level1.value[0].id);
+            // 初始显示文档信息
+            doc.value = level1.value[0];
+          }
+        } else {
+          message.error(data.message);
         }
-      } else {
-        message.error(data.message);
-      }
-    });
+      });
+    };
 
+    //点赞
+    const vote = () => {
+      axios.get('/doc/vote/' + doc.value.id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          doc.value.voteCount++;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
     onMounted(() => {
       handleQuery();
     })
-
     return {
       level1,
       html,
       onSelect,
       defaultSelectedKeys,
-      doc
+      doc,
+      vote,
+      mode: 'default',
+      handleCreated
     }
-  };
-}
+  }
 })
 ;
 
 </script>
 
 <style scoped>
+
+.show table {
+  border-top: 1px solid #ccc;
+  border-left: 1px solid #ccc;
+}
+.show table td,
+.show table th {
+  border-bottom: 1px solid #ccc;
+  border-right: 1px solid #ccc;
+  padding: 3px 5px;
+}
+.show table th {
+  border-bottom: 2px solid #ccc;
+  text-align: center;
+}
+
+/* blockquote 样式 */
+.show blockquote {
+  display: block;
+  border-left: 8px solid #d0e5f2;
+  padding: 5px 10px;
+  margin: 10px 0;
+  line-height: 1.4;
+  font-size: 100%;
+  background-color: #f1f1f1;
+}
+
+/* code 样式 */
+.show code {
+  display: inline-block;
+  *display: inline;
+  *zoom: 1;
+  background-color: #f1f1f1;
+  border-radius: 3px;
+  padding: 3px 5px;
+  margin: 0 3px;
+}
+.show pre code {
+  display: block;
+}
+
+/* ul ol 样式 */
+.show ul, ol {
+  margin: 10px 0 10px 20px;
+}
+
+/* 和antdv p冲突，覆盖掉 */
+.show blockquote p {
+  font-family:"YouYuan";
+  margin: 20px 10px !important;
+  font-size: 16px !important;
+  font-weight:600;
+}
+
+/* 点赞 */
+.vote-div {
+  padding: 15px;
+  text-align: center;
+}
+
+/* 图片自适应 */
+.show img {
+  max-width: 100%;
+  height: auto;
+}
+
+/* 视频自适应 */
+.show iframe {
+  width: 100%;
+  height: 400px;
+}
 
 </style>
